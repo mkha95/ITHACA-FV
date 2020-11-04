7f927dca03de72b612de710f104e3beb0c14b73b
moaad.khamlich <moaad.khamlich@mail.polimi.it>
2020-11-04 22:30:51 +0100

    added bash script to change mesh

---
+++ tutorials/CFD/19bifurcation_SNN/mesh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$1" = "coarse" ]
+then
+    echo "creating coarse mesh"
+    cp utilities/meshes/coarse_mesh/coanda_mesh_cor.msh .
+    gmshToFoam coanda_mesh_cor.msh
+    cp utilities/meshes/coarse_mesh/boundary constant/polyMesh/
+    rm coanda_mesh_cor.msh
+    paraFoam &
+else
+    if [ "$1" = "refined" ]
+    then
+        echo "creating coarse mesh"
+        cp utilities/meshes/fine_mesh/coanda_mesh_ref.msh .
+        gmshToFoam coanda_mesh_ref.msh
+        cp utilities/meshes/fine_mesh/boundary constant/polyMesh/
+        rm coanda_mesh_ref.msh
+        paraFoam &
+    else
+        echo available option: coarse refined
+    fi
+fi
