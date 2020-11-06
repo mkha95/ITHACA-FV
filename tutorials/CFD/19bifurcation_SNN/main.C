fa84719008a741ec17c93e9b081cae8b763ca3c5
moaad.khamlich <moaad.khamlich@mail.polimi.it>
2020-11-07 00:07:14 +0100

    changing interface of the online solver for the bifurcationROM class

--- tutorials/CFD/19bifurcation_SNN/main.C
+++ tutorials/CFD/19bifurcation_SNN/main.C
@@ -42,35 +42,13 @@ int main(int argc,char * argv[]){
     vel_now(0, 0) = 1;
 
     BifurcationROM<reducedSteadyNS> ridotto(bif, vel_now);
-     // Set the inlet velocity
-     // used only for penalty approach
-     ridotto.tauU = Eigen::MatrixXd::Zero(1, 1);
-     ridotto.tauU(0, 0) = 1e-1;
-     std::cout<<"party"<<std::endl;
-     std::cout<<bif.mu<<std::endl;
 
      // Perform an online solve for the new values of inlet velocities
      for (label k = 0; k < bif.mu.size(); k++)
      {
          // Set the reduced viscosity
-         ridotto.nu = bif.mu(0, k);
-         std::cout<<"no problem ye1"<<std::endl;
-         ridotto.solveOnline();
-         std::cout<<"no problem ye2"<<std::endl;
-         Eigen::MatrixXd tmp_sol(ridotto.y.rows() + 1, 1);
-         tmp_sol(0) = k + 1;
-         tmp_sol.col(0).tail(ridotto.y.rows()) = ridotto.y;
-         ridotto.online_solution.append(tmp_sol);
+         ridotto.solveOnline(bif.mu(0,k));
      }
 
-     // Save the online solution
-     ITHACAstream::exportMatrix(ridotto.online_solution, "red_coeff", "python",
-                                "./ITHACAoutput/red_coeff");
-     ITHACAstream::exportMatrix(ridotto.online_solution, "red_coeff", "matlab",
-                                "./ITHACAoutput/red_coeff");
-     ITHACAstream::exportMatrix(ridotto.online_solution, "red_coeff", "eigen",
-                                "./ITHACAoutput/red_coeff");
-     // Reconstruct and export the solution
-     ridotto.reconstruct(true, "./ITHACAoutput/Reconstruction/");
      return 0;
  }
