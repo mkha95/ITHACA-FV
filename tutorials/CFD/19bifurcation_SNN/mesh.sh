cf7b06b53a747c54f50cb3a97657b2ecc25eecad
moaad.khamlich <moaad.khamlich@mail.polimi.it>
2020-11-13 16:58:07 +0100

    adding structured mesh option in script mesh.sh

--- tutorials/CFD/19bifurcation_SNN/mesh.sh
+++ tutorials/CFD/19bifurcation_SNN/mesh.sh
@@ -2,24 +2,27 @@
 
 
 cp utilities/U 0/U
-if [ "$1" = "coarse" ]
-then
-    echo "creating coarse mesh"
-    cp utilities/meshes/coarse_mesh/coanda_mesh_cor.msh .
-    gmshToFoam coanda_mesh_cor.msh
-    cp utilities/meshes/coarse_mesh/boundary constant/polyMesh/
-    rm coanda_mesh_cor.msh
-    paraFoam &
-else
-    if [ "$1" = "refined" ]
-    then
-        echo "creating refined mesh"
-        cp utilities/meshes/fine_mesh/coanda_mesh_ref.msh .
-        gmshToFoam coanda_mesh_ref.msh
-        cp utilities/meshes/fine_mesh/boundary constant/polyMesh/
-        rm coanda_mesh_ref.msh
-        paraFoam &
-    else
-        echo available option: coarse refined
-    fi
-fi
+
+case $1 in
+"coarse") echo "creating coarse mesh"
+          cp utilities/meshes/coarse_mesh/coanda_mesh_cor.msh .
+          gmshToFoam coanda_mesh_cor.msh
+          cp utilities/meshes/coarse_mesh/boundary constant/polyMesh/
+          rm coanda_mesh_cor.msh
+          paraFoam &
+          ;;
+"refined") echo "creating refined mesh"
+           cp utilities/meshes/coarse_mesh/coanda_mesh_cor.msh .
+           gmshToFoam coanda_mesh_cor.msh
+           cp utilities/meshes/coarse_mesh/boundary constant/polyMesh/
+           rm coanda_mesh_cor.msh
+           paraFoam &
+           ;;
+"structured") echo "creating structured mesh"
+              blockMesh
+              ;;
+*)  echo "invalid option: possible options= coarse    refined   structured"
+    ;;
+esac
+
+
