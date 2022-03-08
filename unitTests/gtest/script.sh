58b287c71dba45f568bd8af0fc2338bdc3df772e
Moaad Khamlich <moaad.khamlich@sissa.it>
2022-03-08 17:46:01 +0100

    install gtest

--- unitTests/gtest/script.sh
+++ unitTests/gtest/script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 source /usr/lib/openfoam/openfoam2106/etc/bashrc && \
-    source etc/bashrc && \
-    ./Allwmake -au -j 4;
+source etc/bashrc && \
+#./Allwmake -au -j 4 && \
+apt-get update && \
+apt-get install -y cmake git && \
+git clone https://github.com/google/googletest.git && \
+cd googletest && \
+mkdir build           && \
+cd build && \
+cmake ..              && \
+make && \
+sudo make install;
+
 
